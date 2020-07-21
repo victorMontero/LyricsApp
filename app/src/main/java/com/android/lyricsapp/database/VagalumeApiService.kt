32af1474e393ad202149a11b5a1740c327ce11d6
unknown <viiktoor@gmail.com>
2020-07-21 18:08:43 -0400

    null pointer when i tried to do a request on the API

--- app/src/main/java/com/android/lyricsapp/database/VagalumeApiService.kt
+++ app/src/main/java/com/android/lyricsapp/database/VagalumeApiService.kt
@@ -2,9 +2,12 @@ package com.android.lyricsapp.database
 
 import com.android.lyricsapp.database.response.FeedResponse
 import com.bumptech.glide.manager.ConnectivityMonitor
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,17 +15,15 @@ const val API_KEY = "cba9107da24962656b3cee67c6d029ad"
 
 interface VagalumeApiService {
 
-    @GET("search.art")
+    @GET("artist")
     fun getSearchResponse (
-        @Query("apiKey") apiKey : String,
-        @Query("q") term : String = "drake",
-        @Query("limit") limit : Int
+        //@Query("apiKey") apiKey : String,
+        @Query("q") term : String = "drake"
+        //@Query("limit") limit : Int
     ) : Deferred<FeedResponse>
 
     companion object {
-        operator fun invoke(
-            connectivityInterceptor: ConnectivityInterceptor
-        ): VagalumeApiService {
+        operator fun invoke(): VagalumeApiService {
             val requestInterceptor = Interceptor{chain ->
                 val url = chain.request()
                     .url()
@@ -39,7 +40,16 @@ interface VagalumeApiService {
 
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(requestInterceptor)
-                .addInterceptor(co)
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://www.vagalume.com.br/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(VagalumeApiService::class.java)
         }
     }
 }
--- app/src/main/java/com/android/lyricsapp/modules/feed/FeedFragment.kt
+++ app/src/main/java/com/android/lyricsapp/modules/feed/FeedFragment.kt
@@ -8,6 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.android.lyricsapp.R
+import com.android.lyricsapp.database.API_KEY
+import com.android.lyricsapp.database.VagalumeApiService
+import kotlinx.android.synthetic.main.feed_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class FeedFragment : Fragment() {
 
@@ -28,6 +34,12 @@ class FeedFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = VagalumeApiService()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val artistResponse = apiService.getSearchResponse( "drake").await()
+            frame_layout_text_view.text = artistResponse.artist.lyrics.toString()
+        }
     }
 
 }
